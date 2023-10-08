e3f05a2b841c81fed61409a5dab155c701a963e4
edolor <aghoghomena3@gmail.com>
2023-10-08 09:47:29 -0700

    Updated tailwind config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,33 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'serif': ['"Bungee Inline"'],
+      'sans': ['KoHo']
+    },
+
     extend: {
+      colors: {
+        primary: "#027373",
+        primaryLight: "#029797",
+        primaryBackground: "rgba(2, 115, 115, 0.2)",
+        primaryAbout: "#CEE0DD",
+        orangeish: "rgba(255, 129, 57, 1)",
+        figma: "#F24E1E",
+        yellowish: "#FFE49A",
+      },
+
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(18rem, 24rem))',
+      },
+      
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
